203c1ec34edb9cf96c7dfe7a67eee2262e3d0b45
Ratnesh-Coder <rattneshkumar2024@gmail.com>
2025-04-24 22:02:50 +0530

    Learning

--- sum2ndRow.java
+++ sum2ndRow.java
@@ -1,5 +1,5 @@
 public class sum2ndRow {
-    public static int sum2ndRow (int[][] matrix) {
+    public static int sum (int[][] matrix) {
         int sum = 0;
         for (int j=0; j<matrix[0].length; j++) {
             sum += matrix[1][j];
@@ -8,6 +8,6 @@ public class sum2ndRow {
     }
     public static void main (String args[]) {
         int[][] matrix =  {{1, 4, 9}, {11, 4, 3}, {2, 2,3 }};
-        System.out.println("Sum of 2nd row of matrix is: " + sum2ndRow(matrix));
+        System.out.println("Sum of 2nd row of matrix is: " + sum(matrix));
     }
 }
